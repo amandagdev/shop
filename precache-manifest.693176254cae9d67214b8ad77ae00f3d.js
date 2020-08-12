a950d8298fed502dbc7f0cbcbc6ab83f45ff865a
Amanda Gomes <amandagdev>
2020-08-11 22:29:36 -0300

    Updates

---
+++ precache-manifest.693176254cae9d67214b8ad77ae00f3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8a53d503916c2a95c80f0ba1a0a5f95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "266e96f259c9dcf1aff7",
+    "url": "/static/css/main.5cd63568.chunk.css"
+  },
+  {
+    "revision": "3114c4639146a26fd7fb",
+    "url": "/static/js/2.240bf611.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.240bf611.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "266e96f259c9dcf1aff7",
+    "url": "/static/js/main.609ce5c4.chunk.js"
+  },
+  {
+    "revision": "63ba08987322a76962dc",
+    "url": "/static/js/runtime-main.ec26715b.js"
+  },
+  {
+    "revision": "93895f2a1fbd3d3d2dadb509e0de6bab",
+    "url": "/static/media/cart.93895f2a.svg"
+  }
+]);
